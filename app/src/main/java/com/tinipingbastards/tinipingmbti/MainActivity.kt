b6b62b08d9348944a9a616cf9ecad234117b502d
code-panic <32615067+phs1234@users.noreply.github.com>
2024-11-15 21:38:56 +0900

    메인 화면전환 추가

--- app/src/main/java/com/tinipingbastards/tinipingmbti/MainActivity.kt
+++ app/src/main/java/com/tinipingbastards/tinipingmbti/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.tinipingbastards.tinipingmbti
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.tinipingbastards.tinipingmbti.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener{
+            val intent = Intent(this, QuestionActivity::class.java)
+            startActivity(intent)
         }
     }
 }
